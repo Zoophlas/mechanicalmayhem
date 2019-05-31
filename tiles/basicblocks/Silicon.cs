087d2493245ac4de7574b6893387ccaa35a449d6
Zoophlas <49844938+Zoophlas@users.noreply.github.com>
2019-05-31 11:37:05 -0500

    Moved the silicon tile file out of an unneccesarry folder

---
+++ tiles/basicblocks/Silicon.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace mechanicalmayhem.tiles.basicblocks
+{
+    public class Silicon : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileMergeDirt[Type] = true;
+            Main.tileBlockLight[Type] = true;
+            Main.tileLighted[Type] = true;
+            dustType = mod.DustType("Sparkle");
+            drop = mod.ItemType("SiliconOre");
+            AddMapEntry(new Color(128, 128, 128));
+            minPick = 50;
+            soundType = 21;
+            soundStyle = 1;
+            TileID.Sets.Ore[Type] = true;
+            Main.tileSpelunker[Type] = true;
+            Main.tileValue[Type] = 410;
+            Main.tileShine2[Type] = true;
+        }
+
+        public override void NumDust(int i, int j, bool fail, ref int num)
+        {
+            num = fail ? 1 : 3;
+        }
+
+        public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)
+        {
+            r = 0.5f;
+            g = 0.5f;
+            b = 0.5f;
+        }
+    }
+}
