890906c7278030142025682e1cef7ef7338ff45d
Zoophlas <49844938+Zoophlas@users.noreply.github.com>
2019-06-01 19:49:00 -0500

    Added the electric hamaxe and it's placeholder texture

---
+++ Items/Tools/ElectricHamaxe.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace mechanicalmayhem.Items.Tools
+{
+    public class ElectricHamaxe : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 40;
+            item.height = 40;
+            item.melee = true;
+            item.damage = 20;
+            item.axe = 20;
+            item.hammer = 60;
+            item.useTime = 16;
+            item.useAnimation = 16;
+            item.useStyle = 1;
+            item.knockBack = 6;
+            item.value = 100000;
+            item.rare = 2;
+            item.UseSound = SoundID.Item1;
+            item.autoReuse = true;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(null, "SiliconWafer", 10);
+            recipe.AddIngredient(ItemID.GoldBar, 4);
+            recipe.AddIngredient(ItemID.CopperBar, 2);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
