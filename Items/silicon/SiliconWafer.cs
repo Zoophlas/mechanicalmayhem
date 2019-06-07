c4453426981635ea0c60977aeea3f9524fb2e7a8
Zoophlas <49844938+Zoophlas@users.noreply.github.com>
2019-06-06 22:45:09 -0500

    Changed max stack

--- Items/silicon/SiliconWafer.cs
+++ Items/silicon/SiliconWafer.cs
@@ -15,7 +15,7 @@ namespace mechanicalmayhem.Items.silicon
         {
             item.width = 20;
             item.height = 20;
-            item.maxStack = 999;
+            item.maxStack = 99;
             item.value = 750;
         }
 
