094b41f5025c69b7b7b908810a22d92e2cf854d3
Zoophlas <49844938+Zoophlas@users.noreply.github.com>
2019-06-06 22:45:39 -0500

    Added the SpeedModule
    Co-Authored-By: dasy21 <dasy21@users.noreply.github.com>

---
+++ Items/SpeedModule.cs
@@ -0,0 +1,34 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace mechanicalmayhem.Items
+{
+    public class SpeedModule : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Can be combined with other items to increase speed.");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 20;
+            item.height = 20;
+            item.maxStack = 99;
+            item.value = 500; // Change to 5 silver if current value is not 5 silver
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(null, "SiliconWafer", 3);
+            recipe.AddIngredient(ItemID.CopperBar);
+            recipe.AddIngredient(ItemID.GoldBar);
+            recipe.AddIngredient(ItemID.PInkGel, 5);
+            recipe.AddTile(null, "SolderingTable");
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
