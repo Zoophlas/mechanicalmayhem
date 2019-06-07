131166267b31e25727e942b593eaf5711f661c39
Zoophlas <49844938+Zoophlas@users.noreply.github.com>
2019-06-06 22:44:47 -0500

    Added SpeedBoots

---
+++ Items/Accessories/Boots/SpeedBoots.cs
@@ -0,0 +1,39 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace mechanicalmayhem.Items.Accessories.Boots
+{
+    [AutoloadEquip(EquipType.Boots)]
+    public class SpeedBoots : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Non magic way for immense speed. Faster too!");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 32;
+            item.height = 28;
+            item.value = 300000;
+            item.rare = 2;
+            item.accessory = true;
+            item.name = "Speed Boots";
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.moveSpeed = 1.62f;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(null, "BasicBoots");
+            recipe.AddIngredient(null, "SpeedModule");
+            recipe.AddTile(null, "SolderingTable");
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
