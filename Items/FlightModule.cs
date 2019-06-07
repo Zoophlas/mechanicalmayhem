24f6e47cd353e7348b1020d3f68e7388aed89a26
Zoophlas <49844938+Zoophlas@users.noreply.github.com>
2019-06-07 01:44:02 -0500

    Added the Flight Module

---
+++ Items/FlightModule.cs
@@ -0,0 +1,35 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace mechanicalmayhem.Items
+{
+    public class FlightModule : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Can be combined with other items to allow flight.");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 20;
+            item.height = 20;
+            item.maxStack = 99;
+            item.value = 5000;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(null, "SiliconWafer", 3);
+            recipe.AddIngredient(ItemID.CopperBar);
+            recipe.AddIngredient(ItemID.GoldBar);
+            recipe.AddIngredient(ItemID.SoulOfFlight, 5);
+            recipe.AddIngredient(ItemID.Feather, 3);
+            recipe.AddTile(null, "SolderingTable");
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
