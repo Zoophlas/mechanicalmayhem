5db06a6548e1b450c354a51b418fedf08fdd484e
Zoophlas <49844938+Zoophlas@users.noreply.github.com>
2019-06-08 15:34:32 -0500

    Updated tile data in C# file

--- tiles/SolderingTable.cs
+++ tiles/SolderingTable.cs
@@ -10,13 +10,13 @@ namespace mechanicalmayhem.tiles
     {
         public override void SetDefaults()
         {
-            Main.tileSolidTop[Type] = true;
+            Main.tileSolidTop[Type] = false;
             Main.tileFrameImportant[Type] = true;
             Main.tileNoAttach[Type] = true;
             Main.tileTable[Type] = true;
             Main.tileLavaDeath[Type] = true;
-            TileObjectData.newTile.CopyFrom(TileObjectData.Style2x1); // Change to 3x2 when there is a permenant texture
-            TileObjectData.newTile.CoordinateHeights = new int[] { 18 };
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x2);
+            TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16 };
             TileObjectData.addTile(Type);
             ModTranslation name = CreateMapEntryName();
             name.SetDefault("Soldering Table");
@@ -26,7 +26,7 @@ namespace mechanicalmayhem.tiles
 
         public override void KillMultiTile(int i, int j, int frameX, int frameY)
         {
-            Item.NewItem(i * 16, j * 16, 32, 16, mod.ItemType("SolderingTable"));
+            Item.NewItem(i * 16, j * 16, 48, 32, mod.ItemType("SolderingTable"));
         }
     }
 }
